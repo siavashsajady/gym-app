d14f2e8d7e3316e3f01cb108699ae728b8dac243
siavashsajady <hubgitpro@gmail.com>
2022-06-18 16:21:50 +0200

    Add Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
+import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width='400px'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
