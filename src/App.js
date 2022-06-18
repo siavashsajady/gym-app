2836dbc889f385cfce6d991e9e14a2f456499fe9
siavashsajady <hubgitpro@gmail.com>
2022-06-18 16:29:37 +0200

    Finalize index.js, Fixed Bug

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <Box width='400px'>
+    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
       <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
