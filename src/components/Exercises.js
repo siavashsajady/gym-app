a306d157e4ca33717a7ac6ac24af5c4f2f69ea02
siavashsajady <hubgitpro@gmail.com>
2022-06-21 10:23:39 +0200

    Implement ExerciseCard

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Pagination } from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   console.log(exercises);
@@ -17,7 +18,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         justifyContent='center'
       >
         {exercises.map((exercise, index) => (
-          <p>{exercise.name}</p>
+          <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
     </Box>
